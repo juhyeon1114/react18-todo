c17a178a39c82353d408a8720d2449809da72803
juhyeon1114 <juhyeon1114@gmail.com>
2022-09-12 11:19:24 +0900

    update : Setup tailwind

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function Form({value, handleChange, onSubmitForm}) {
     return (
-        <form onSubmit={e => onSubmitForm(e)}>
-            <input type="text" name="value" placeholder='할 일을 입력하세요.' value={value} onChange={e => handleChange(e)} />
-            <input type="submit" name="value" value="입력" />
+        <form className='flex pt-2' onSubmit={e => onSubmitForm(e)}>
+            <input className='w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow' type="text" name="value" placeholder='할 일을 입력하세요.' value={value} onChange={e => handleChange(e)} />
+            <input className='p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400' type="submit" name="value" value="입력" />
         </form>
     )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -34,10 +34,14 @@ export default function List({todoData, setTodoData}) {
 
     return (
         todoData.map(todo => (
-            <div style={getStyle()} key={todo.id}>
-                <input type="checkbox" checked={todo.completed} onChange={e => handleCheck(e, todo.id)} />
-                <span className={todo.completed ? 'done' : ''}>{todo.title}</span>
-                <button style={btnStyle} onClick={() => handleClick(todo.id)}>X</button>
+            <div key={todo.id} className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+                <div className="flex items-center gap-1">
+                    <input type="checkbox" checked={todo.completed} onChange={e => handleCheck(e, todo.id)} />
+                    <span className={todo.completed ? 'line-through' : ''}>{todo.title}</span>
+                </div>
+                <div className="items-center">
+                    <button onClick={() => handleClick(todo.id)}>❌</button>
+                </div>
             </div>
         ))
     )
